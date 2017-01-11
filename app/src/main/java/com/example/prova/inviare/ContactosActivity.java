142c8ab2f7ff31dda7f00b361724559df65eb360
ToniCosta96 <ToniCosta96@gmail.com>
2017-01-11 23:51:21 +0100

    Ahora en contactos se carga una lista con los contactos del teléfono. (Error: salen duplicados)

--- app/src/main/java/com/example/prova/inviare/ContactosActivity.java
+++ app/src/main/java/com/example/prova/inviare/ContactosActivity.java
@@ -1,13 +1,25 @@
 package com.example.prova.inviare;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.prova.inviare.adapters.AdaptadorConversaciones;
 import com.example.prova.inviare.elementos.Contacto;
@@ -15,7 +27,9 @@ import com.example.prova.inviare.elementos.Contacto;
 import java.util.ArrayList;
 
 public class ContactosActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CONTACT = 1;
     private ArrayList<Contacto> arrayContactos;
+    private AdaptadorConversaciones adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +42,109 @@ public class ContactosActivity extends AppCompatActivity {
         arrayContactos.add(new Contacto("Contacto1","sub1","1"));
         arrayContactos.add(new Contacto("Contacto2","sub2","2"));
 
-        // specify an adapter (see also next example)
-        final AdaptadorConversaciones adaptador = new AdaptadorConversaciones(this,arrayContactos);
+        // specify an adapter
+        adaptador = new AdaptadorConversaciones(this,arrayContactos);
         recyclerView.setAdapter(adaptador);
         // use a linear layout manager
         recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+        //Pedir permisos para ver los contactos
+        askForContactPermission();
+    }
+
+    private void anyadirContactos(){
+        //Anyade los contactos del teléfono al arrayContactos después de eliminar los anteriores.
+        arrayContactos.clear();
+        final String[] PROJECTION = new String[] {
+                ContactsContract.RawContacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.PHOTO_URI,
+                ContactsContract.CommonDataKinds.Phone.NUMBER,
+                ContactsContract.CommonDataKinds.Photo.CONTACT_ID };
+        final String filtro = ""+ ContactsContract.Contacts.HAS_PHONE_NUMBER + " > 0 and " + ContactsContract.CommonDataKinds.Phone.TYPE+"="+ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE;
+        final String orden = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC";
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, PROJECTION,filtro,null, orden);
+
+        if (phones != null) {
+            while (phones.moveToNext()) {
+                String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                arrayContactos.add(new Contacto(name,phoneNumber,"MÓVIL¿?"));
+            }
+            phones.close();
+        }
+        //Actualizar adaptador
+        adaptador.notifyDataSetChanged();
+    }
+
+    public void askForContactPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_CONTACTS)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Contacts access needed");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("Please confirm Contacts access");
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {Manifest.permission.READ_CONTACTS}
+                                    , PERMISSION_REQUEST_CONTACT);
+                        }
+                    });
+                    builder.show();
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_CONTACTS},
+                            PERMISSION_REQUEST_CONTACT);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }else{
+                anyadirContactos();
+            }
+        }
+        else{
+            anyadirContactos();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CONTACT: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    anyadirContactos();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    Toast.makeText(this, "No Permissions ", Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
@@ -47,7 +159,10 @@ public class ContactosActivity extends AppCompatActivity {
         Intent i;
         switch (item.getItemId()) {
             case R.id.item_actualizar:
-                //
+                //Vuelve a cargar los contactos
+                askForContactPermission();
+                adaptador.notifyDataSetChanged();
+                Toast.makeText(getApplicationContext(),"La lista de contactos ha sido actualizada.",Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.item_ajustes:
                 //
